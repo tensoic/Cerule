cbe2d17638e4a1a52f08c625461bea687f447900
Adarsh Shirawalmath <114558126+adarshxs@users.noreply.github.com>
2024-05-27 16:01:24 +0530

    add reference to HF

--- cerule/model/language_model/gemma/tokenization_gemma.py
+++ cerule/model/language_model/gemma/tokenization_gemma.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Imported from Huggingface Transformers - transformers.models.gemma
+
 """Tokenization classes for Gemma."""
 
 import os
