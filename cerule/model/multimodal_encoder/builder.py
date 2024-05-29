caea95247ec436147d52880ee4f5329324e01621
Adarsh Shirawalmath <114558126+adarshxs@users.noreply.github.com>
2024-05-29 11:54:47 +0530

    Add S^2 support

--- cerule/model/multimodal_encoder/builder.py
+++ cerule/model/multimodal_encoder/builder.py
@@ -1,20 +1,13 @@
 import os
-from .eva_clip.eva_clip_encoder import EvaClipVisionTower
-from .siglip.siglip_encoder import SigLipVisionTower
-from .clip.clip_encoder import CLIPVisionTower
-
+from .siglip.siglip_encoder import SigLipVisionTower, SiglipVisionTowerS2
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
 
     if 'sig' in vision_tower.lower():
-        return SigLipVisionTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
-
-    elif 'eva' in vision_tower.lower():
-        return EvaClipVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-
-    elif 'clip' in vision_tower.lower():
-        return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-
+        if use_s2:
+            return SiglipVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
+        else:
+            return SiglipVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
     else:
         raise ValueError(f'Unknown vision tower: {vision_tower}')
